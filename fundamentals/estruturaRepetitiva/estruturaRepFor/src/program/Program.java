4893c357fe8683eb47ab772256be066ccdf24a86
Rebecca Costal <rebeccacostal@hotmail.com>
2024-01-25 20:14:49 -0300

    Exercicio de For

--- fundamentals/estruturaRepetitiva/estruturaRepFor/src/program/Program.java
+++ fundamentals/estruturaRepetitiva/estruturaRepFor/src/program/Program.java
@@ -1,5 +1,22 @@
-public class App {
+package program;
+
+import java.util.Scanner;
+
+public class Program {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner sc = new Scanner (System.in);
+
+        // Quando se sabe previamente quantas vezes repetir a condição
+        int n = sc.nextInt();
+        int soma = 0;
+        
+        for (int i = 0; i<n; i++){
+            int x = sc.nextInt();
+            soma += x;
+        }
+        System.out.println("Soma: " + soma);
+
+        sc.close();
     }
 }
