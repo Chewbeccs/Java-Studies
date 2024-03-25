3a2dae211fed297a350ad3ddc9bd97fdecfddbd9
Rebecca Costal <rebeccacostal@hotmail.com>
2024-03-25 19:50:16 -0300

    Reorganização das pastas de estudos fundamentals

--- fundamentals/Estrutura Condicional/exeIfElse04/src/program/Program.java
+++ fundamentals/Estrutura Condicional/exeIfElse04/src/program/Program.java
@@ -17,11 +17,20 @@ public class Program {
         System.out.println("Horario do fim de jogo: ");
         fim = sc.nextInt();
 
-        if (inicio < fim) {
+
+        /*Ler a condição do If para entender.
+        Se a hora inicial for menor que a hora final (dentro das 24h)
+        ou seja ex: inicio ás 2h e fim às 18, dentro das 24h vc calcula o maior
+        menos o menor*/
+        if (inicio < fim) {         
             total = fim - inicio;                       
         }
+         /*Ler a condição do If para entender.
+        Se a hora inicial for maior que a hora final (dentro das 24h)
+        ou seja ex: inicio ás 12h e fim às 5, dentro das 24h vc calcula o horario de inicio
+        - às 24h (ficaria no formato de horas 12h) e com o resultado soma com o horario final*/
         else{
-            total = 24 - inicio + fim;           
+            total = 48 - inicio + fim;           
         }
         
         System.out.println("O jogo durou " + total + " hora(s)");
